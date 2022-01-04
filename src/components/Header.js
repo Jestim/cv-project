847e776d87b3e4287b9f15b1d343a925f48bc555
Jestim <jesper.os.thiman@gmail.com>
2022-01-04 11:18:46 +0100

    Environmetn set up and header created

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+class Header extends react.Component {
+	render () {
+		return (
+			<div className='header'>
+				<h1 className='header-text'>
+					CV
+				</h1>
+			</div>
+		);
+	}
+}
+
+export default Header;
